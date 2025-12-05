92ffa41147dbaf8cbb5bfdd0e0e46c88f1c24ea7
lopezsanchezabraham46-ai <lopez.sanchez.abraham.46@cbtis118.edu.mx>
2025-12-05 11:18:48 -0600

    Create practica3.js

---
+++ practica3.js
@@ -0,0 +1,21 @@
+use("bd_py");
+
+
+db.balon.deleteOne({ Modelo: "Copa América 2024" });
+db.balon.deleteOne({ Modelo: "MLS Pro" });
+
+
+db.Zapatos.deleteOne({ Modelo: "Yeezy Boost 350 V2" });
+db.Zapatos.deleteOne({ Modelo: "Curry Flow 10" });
+
+
+db.TELEVISIONES.deleteOne({ Modelo: "OLED C3" });
+db.TELEVISIONES.deleteOne({ Modelo: "U7K" });
+
+
+db.Audifonos.deleteOne({ Modelo: "JBL Live Pro 3" });
+db.Audifonos.deleteOne({ Modelo: "AirPods Max 2" });
+
+
+db.Consolas.deleteOne({ Modelo: "ROG Ally X" });
+db.Consolas.deleteOne({ Modelo: "Ayaneo Kun" });
