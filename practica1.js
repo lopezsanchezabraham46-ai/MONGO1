b2c7e306a2e18e2c81c0182761d21eeb44a1298a
lopezsanchezabraham46-ai <lopez.sanchez.abraham.46@cbtis118.edu.mx>
2025-12-05 11:17:50 -0600

    Create practica1.js

---
+++ practica1.js
@@ -0,0 +1,60 @@
+use("bd_py");
+
+
+db.balon.find({ Marca: "Adidas" });
+db.balon.find({ Marca: "Nike Football" });
+db.balon.find({ Modelo: "LaLiga Orbita" });
+db.balon.find({ Año: 2024 });
+db.balon.find({ Marca: "Voit" });
+db.balon.find({ Año: 2025 });
+db.balon.find({ Modelo: "Bundesliga Brillant APS" });
+db.balon.find({ Marca: "Puma" });
+db.balon.find({ Modelo: "UEFA Champions League Pro" });
+db.balon.find({ Marca: "Derbystar" });
+
+
+db.Zapatos.find({ Marca: "Nike Inc." });
+db.Zapatos.find({ Modelo: "Ultraboost Light" });
+db.Zapatos.find({ Marca: "Adidas" });
+db.Zapatos.find({ Marca: "Puma" });
+db.Zapatos.find({ Modelo: "NB 990v6" });
+db.Zapatos.find({ Marca: "Under Armour" });
+db.Zapatos.find({ Modelo: "Zoom Freak 5" });
+db.Zapatos.find({ Marca: "Converse" });
+db.Zapatos.find({ Año: 2025 });
+db.Zapatos.find({ Modelo: "Forum Low" });
+
+
+db.TELEVISIONES.find({ Marca: "LG Electronics" });
+db.TELEVISIONES.find({ Modelo: "Neo QLED 8K QN900D" });
+db.TELEVISIONES.find({ Marca: "Sony" });
+db.TELEVISIONES.find({ Marca: "TCL" });
+db.TELEVISIONES.find({ Modelo: "U8K MiniLED" });
+db.TELEVISIONES.find({ Año: 2024 });
+db.TELEVISIONES.find({ Marca: "Hisense" });
+db.TELEVISIONES.find({ Año: 2025 });
+db.TELEVISIONES.find({ Modelo: "A90J OLED" });
+db.TELEVISIONES.find({ Marca: "Samsung" });
+
+
+db.Audifonos.find({ Marca: "Apple Inc." });
+db.Audifonos.find({ Modelo: "WH-1000XM6" });
+db.Audifonos.find({ Marca: "Samsung" });
+db.Audifonos.find({ Marca: "Bose" });
+db.Audifonos.find({ Modelo: "Beats Studio Pro" });
+db.Audifonos.find({ Marca: "Nothing" });
+db.Audifonos.find({ Marca: "Sennheiser" });
+db.Audifonos.find({ Modelo: "Soundcore Liberty 5" });
+db.Audifonos.find({ Año: 2025 });
+db.Audifonos.find({ Marca: "JBL" });
+
+db.Consolas.find({ Marca: "Sony Interactive Entertainment" });
+db.Consolas.find({ Modelo: "Xbox Series X2" });
+db.Consolas.find({ Marca: "Nintendo" });
+db.Consolas.find({ Marca: "Valve" });
+db.Consolas.find({ Modelo: "ROG Ally X" });
+db.Consolas.find({ Año: 2024 });
+db.Consolas.find({ Marca: "Lenovo" });
+db.Consolas.find({ Modelo: "Ayaneo Kun" });
+db.Consolas.find({ Año: 2025 });
+db.Consolas.find({ Marca: "Meta" });
